1423c57389cd8ec840c212678386179172b2125e
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2019-09-28 14:06:31 -0300

    Promo code model

---
+++ Models/PromoCode.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace promoCode.Models
+{
+    public class PromoCode
+    {
+        public long Id { get; set; }        
+        public string Description { get; set; }         
+        public DateTime ExpireDate { get; set; }
+        public int Amount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
